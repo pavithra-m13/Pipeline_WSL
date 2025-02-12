300992db43f74e88687280ad8cf6b6e8ee31c505
pavithra-m13 <138682484+pavithra-m13@users.noreply.github.com>
2025-02-12 16:11:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,14 @@
 provider "local" {}
 
 resource "null_resource" "setup_apache" {
-  interpreter = ["bash", "-c"]
   provisioner "local-exec" {
-    command = <<EOT
+    command = "bash -c <<EOT
       # Update packages, install Apache, enable, and start the service
       echo '' | sudo -S apt update -y && \
       echo '' | sudo -S apt install -y apache2 && \
       sudo systemctl enable apache2 && \
       sudo systemctl start apache2
-    EOT
+    EOT"
   }
 }
 
