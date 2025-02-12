27c07db48c020eb6ed7c2aa830c2d021fe2b7965
pavithra-m13 <138682484+pavithra-m13@users.noreply.github.com>
2025-02-12 15:29:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ resource "null_resource" "setup_apache" {
   provisioner "local-exec" {
     command = <<EOT
       # Update packages, install Apache, enable, and start the service
-      sudo apt update -y && \
-      sudo apt install -y apache2 && \
+      echo '' | sudo -S apt update -y && \
+      echo '' | sudo -S apt install -y apache2 && \
       sudo systemctl enable apache2 && \
       sudo systemctl start apache2
     EOT
@@ -18,7 +18,7 @@ resource "null_resource" "deploy_website" {
   provisioner "local-exec" {
     command = <<EOT
       # Deploy website files
-      sudo cp -r ${path.module}/website/* /var/www/html/
+      echo '' | sudo -S cp -r ${path.module}/website/* /var/www/html/
       sudo systemctl restart apache2
     EOT
   }
